3fbbc9c01aba94cd7143aa3ff3ee9a9dcf12dcbb
who asks? <user@localhost>
2026-06-30 01:16:10 -0600

    moved

--- bcur/README.md
+++
@@ -1,2 +0,0 @@
-vendored from https://github.com/BlockchainCommons/bc-ur
-2bfc3fd396498c2519273aeaa732abf7ea7d24b8
--- windowmodaldialog.cpp
+++
@@ -1,9 +0,0 @@
-#include "windowmodaldialog.h"
-
-WindowModalDialog::WindowModalDialog(QWidget *parent)
-    : QDialog(parent)
-{
-#ifndef Q_OS_MACOS
-    this->setWindowModality(Qt::WindowModal);
-#endif
-}
